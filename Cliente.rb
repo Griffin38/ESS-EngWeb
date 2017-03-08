554a5d0a93086a6058b70fdec050ddd39288cd05
Jorge Simao <schroeder.jorge@gmail.com>
2017-03-08 19:44:59 +0000

    Base do cliente, metodo de simulacao

--- Cliente.rb
+++ Cliente.rb
@@ -16,7 +16,7 @@ when "login"
 	server.puts "#{password}"
 	resposta = server.gets
 	if resposta = "OK"
-		### ENVIAR VALORES
+		simula()
 	else server.close
 	end	
 
@@ -30,8 +30,53 @@ when "registo"
 	password = gets.chomp
 	server.puts "#{password}"
 
+	if resposta = "OK"
+		simula()
+	else server.close
+	end
+
 else puts "Opcao invalida!"
 
 end
 
-server.close
+def simula()
+puts "PARA TERMINAR ESCREVA: CLOSE"
+@temp = 0
+@ruido = 0
+
+   Thread.new {
+   loop
+   sleep(30)
+   @temp++
+   value = rand(-40..80)
+   latitude = rand(-90.000000000...90.000000000)
+   longitude = rand(-180.000000000...180.000000000)
+   time = Time.now.getutc
+   server.puts "Nome: #{login} | Tipo: Temperatura | Valor: #{value} | GPS: #{latitude}, #{longitude} | Timestamp: #{time}"
+   end
+   }   
+   
+   Thread.new {
+   loop
+   sleep(1)
+   @ruido++
+   value = rand(0..200)
+   latitude = rand(-90.000000000...90.000000000)
+   longitude = rand(-180.000000000...180.000000000)
+   time = Time.now.getutc
+   server.puts "Nome: #{login} | Tipo: Acustica | Valor: #{value} | GPS: #{latitude}, #{longitude} | Timestamp: #{time}"
+   end
+   }   
+
+if gets.chomp == "CLOSE" || gets.chomp == "close"
+	total_leituras = @temp + @ruido
+	server.puts "TOTAL DE LEITURAS: #{total_leituras}"
+	puts "TOTAL DE LEITURAS: #{total_leituras}"
+	server.close
+end
+
+end
+
+
+
+
